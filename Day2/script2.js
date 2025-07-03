05913907591ed02c28fc8d5298fe21d61c106002
sakshi-sakhare <sakshisakhare999@gmail.com>
2025-07-03 22:40:07 +0530

    day 2 & day 3

---
+++ Day2/script2.js
@@ -0,0 +1,42 @@
+
+
+//Number
+var vlaue=10;
+console.log(vlaue);
+
+let no=20;
+console.log(no);
+
+const PI=3.14;
+console.log(PI);
+console.log(typeof PI); //typeof Number
+
+//string
+const USER_NAME="Epl"
+console.log(USER_NAME)
+
+let name="sakshi"
+console.log(name);
+console.log(typeof name);
+
+let letter='A';
+console.log(letter);
+console.log(typeof letter); // typeof string
+
+// Boolean
+let IS_USER_LOGIN = true
+console.log(IS_USER_LOGIN)
+
+let IS_OTP_VEIREY = false
+console.log(IS_OTP_VEIREY)
+console.log(typeof IS_OTP_VEIREY) // typeof boolean
+
+//Undefined 
+let abc
+console.log(abc)
+console.log(typeof abc)   //typeof undefined
+
+//Null
+let xyz=null
+console.log(xyz)
+console.log(typeof xyz)    //typeof object
---
+++ Day3/script3.js
@@ -0,0 +1,84 @@
+//arithmatic Oparator +,-.*,/,%
+//+ addition
+
+let abc = 10;
+let xyz= 20;
+c=abc+xyz;
+console.log("10+20=",c);
+
+//- subtraction
+
+let a=200;
+let b=100;
+console.log(a-b);
+
+//* multipication
+let mul=200;
+let multi=200;
+console.log("",mul*multi);
+
+// / divison
+
+let div=20;
+let del=2;
+console.log(div/del);
+
+// % mode
+
+let mod=10;
+let rem=3;
+console.log(mod%rem);
+
+//assignment oprator +=,-=,*=,/=
+
+// = 
+let no=10;
+console.log(no);
+
+//+=
+let x=10;
+ x+=10;
+console.log(x);
+
+//-=
+let y=50;
+y-=10;
+console.log(y);
+
+//*=
+
+let d=10;
+d*=200;
+console.log(d);
+
+// /=
+let ok=50;
+ok/=2;
+console.log(ok);
+
+//comparision operator == ,=== , != , !===
+
+let first=10
+let second=10
+console.log(first==second)
+
+
+let first_no=10;
+let second_no=10;
+console.log(first_no!=second_no);
+
+let m=10;
+let n='10';
+console.log(m===n);
+
+let o='sakshi'; //small letter
+let p='Sakshi'; //Capital letter
+console.log(o!==p);
+
+
+
+
+
+
+
+
